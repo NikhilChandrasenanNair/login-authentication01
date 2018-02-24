90273a8353c038fde2a392d6c85e4882334a603a
MCHQ\nchandrasenan <nchandrasenan@mcfadyen.com>
2018-02-24 23:17:38 +0530

    saving to the database

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -16,7 +16,7 @@ userController.register = (req, res) => {
       if (err) {
         return res.json({success: false, msg: 'Username already exists.'});
       }
-      res.json({success: true, msg: 'Successful created new user.'});
+      res.json({success: true, msg: 'Successfully created new user.'});
     });
   }
 }
