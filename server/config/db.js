80901bb260914025ae75d13f5c5cf4a10823d887
MCHQ\nchandrasenan <nchandrasenan@mcfadyen.com>
2018-02-24 22:02:40 +0530

    setting up mongoose

---
+++ server/config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MONGO_CONNECT_URL:"mongodb://root:root@ds247678.mlab.com:47678/login-authentication01"
+};
--- server/src/app.js
+++ server/src/app.js
@@ -5,8 +5,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+const db = require('../config/db')
 
 const app = express()
+
+mongoose.connect(db.MONGO_CONNECT_URL)
+
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
@@ -17,4 +22,10 @@ app.post('/register', (req, res) => {
   })
 })
 
-app.listen(process.env.PORT || 8081)
+app.listen(process.env.PORT || 8081, function (err) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('Server Running')
+  }
+})
